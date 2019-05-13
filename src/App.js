2897b5afcf95b98741a7b7dad9662d076c0075de
zhang_shuai_yang <1529060156@qq.com>
2019-05-13 17:56:48 +0800

    xss

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
+import { HashRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Home from './view/home/Home';
+import Login from './view/home/Login';
+
+class App extends React.Componet  {
+  sander() {
+    return (
+      <div className="app">
+        <Router>
+          <div>
+            <Route path="/" componet={Home}></Route>
+            <Route path="/home" componet={Home}></Route>
+          </div>
+        </Router>
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/view/login/Login.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default class Login extends React.Componet {
+  
+  constructor(props) {
+    super(props)
+  }
+  
+  rander() {
+    return (
+      <div className="Login">
+
+        Login
+      </div>
+    )
+  }
+} 
